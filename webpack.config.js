2be5df8995cd575f17dc3ba7c607818532b4c647
Daniel Deutsch <deudan1010@gmail.com>
2017-07-15 23:39:33 +0200

    add advanced flux and redux

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,9 @@ module.exports = {
         ]
     },
     entry: {
-        "cpanel": ["./src/control-panel.js"]
+        cpanel: ["./src/control-panel.js"],
+        "message-board": ["./src/message-board.js"],
+        tasks: ["./src/tasks.js"]
     },
     output: {
         path: path.resolve(__dirname, "public"),
