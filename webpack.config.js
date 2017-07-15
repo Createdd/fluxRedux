717c402881d68874902513c185813dbe3087caa7
Daniel Deutsch <deudan1010@gmail.com>
2017-07-15 20:01:53 +0200

    initial

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+module.exports = {
+    module: {
+        loaders: [
+            {
+                loader: "babel-loader",
+
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+
+                test: /\.js?$/,
+
+                // Options to configure babel with
+                query: {
+                    presets: ['es2015'],
+                    plugins: ['transform-object-rest-spread']
+                }
+            },
+        ]
+    },
+    entry: {
+        "cpanel": ["./src/control-panel.js"]
+    },
+    output: {
+        path: path.resolve(__dirname, "public"),
+        publicPath: "/assets/",
+        filename: "[name].bundle.js"
+    },
+    devServer: { inline: true },
+    devtool: 'source-map',
+}
